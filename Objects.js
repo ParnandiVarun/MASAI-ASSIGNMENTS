20b51e82ee1078055c08ed8d96cf4272060f9d24
Parnandi_ Sricharan_21008490 <parnandivarun@gmail.com>
2025-01-06 23:56:25 +0530

    Js Object file

---
+++ Objects.js
@@ -0,0 +1,9 @@
+let book = {
+  title: "The Hobbit",
+  author: "J.R.R. Tolkien",
+  year: 1937,
+};
+
+console.log("Title : " + book.title);
+console.log("Author : " + book.author);
+console.log("Published Year : " + book.year);
